7c7e8f3b6dee1cb1912d0c6ab80ac1b517485bd0
LittleYenMin <fkym08@gmail.com>
2019-06-25 23:05:24 +0800

    set encoding to utf8.

--- data.py
+++ data.py
@@ -5,15 +5,17 @@ import datetime
 def create(topic: str, headers: [str]):
     ts = datetime.datetime.now().timestamp()
     name = '{topic}-{timestamp}.csv'.format(topic=topic, timestamp=int(ts))
-    with open(name, 'w') as f:
+    with open(name, 'w', encoding='utf-8') as f:
         csv_writer = csv.writer(f)
         csv_writer.writerow(headers)
     return name
 
 
 def store(filename: str, rows: [[str]]):
-    with open(filename, 'a') as f:
+    with open(filename, 'a', encoding='utf-8', newline='') as f:
         csv_writer = csv.writer(f)
         for row in rows:
+            if len(row) <= 0:
+                continue
             csv_writer.writerow(row)
 
